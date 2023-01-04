0e23b89d17050e580eaf3c5e8f83d41a5ba9af10
larbo2022 <119931694+larbo2022@users.noreply.github.com>
2023-01-04 10:02:15 +0100

    new input system emprovement and implementation of the the script for invoke unity events

--- Assets/Scripts/NewPlayerController.cs
+++ Assets/Scripts/NewPlayerController.cs
@@ -11,19 +11,42 @@ public class NewPlayerController : MonoBehaviour
     [SerializeField] float rMax = 20f;
 
     private float movementX;
+    Vector2 movementV2;
 
+   // bool isFeedPressed;
 
     // Fonctions pour les Send Messages
-    public void OnMove(InputValue value)
+      public void OnMove(InputValue value)
+      {
+          movementX = value.Get<float>();
+      }
+
+      public void OnFeed()
+      {
+        Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+
+      }
+         
+
+ /* 
+    public void OnMoveCharacter(InputAction.CallbackContext ctx)
     {
-        movementX = value.Get<float>();
+
+        movementX = ctx.ReadValue<float>();
+        
+
     }
 
-    
+    public void OnFeedAnimal(InputAction.CallbackContext ctx)
+    {
+        isFeedPressed = ctx.ReadValueAsButton();
+
+    }   */
+   
+
     private void Update()
     {
-        //Vector3 movement = new Vector3(movementX, 0f, 0f);
-        //movement.Normalize();
+        
         transform.Translate(Vector3.right * movementX * speedp * Time.deltaTime);
 
         if (transform.position.x < lMax)
@@ -35,9 +58,17 @@ public class NewPlayerController : MonoBehaviour
             transform.position = new Vector3(rMax, transform.position.y, transform.position.z);
         }
 
-        if (Input.GetKeyDown(KeyCode.Space))
+
+    /*    if (isFeedPressed)
         {
             Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
-        }
+            //feedSound.Play();
+            
+        }  */
+
+     /*  if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }  */
     }
 }
